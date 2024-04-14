8d5ef2106aa1b670838c35f85a5975d4ea498742
Evgeniy Petrov <evgeniy161102@gmail.com>
2024-04-14 13:29:30 +0300

    Добавлена информация копирайта футера

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,13 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
   return (
     <footer className={styles["footer"]}>
-      <div className="container">Footer</div>
+      <div className="container">
+        <div className={styles["footer-row"]}>
+          <div className={styles["footer__copyright"]}>
+            © 2015-2019 Fotoprokat24
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
