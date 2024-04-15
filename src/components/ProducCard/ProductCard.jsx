43d4315d9b9659062d477e98c9bb08d351d15385
Evgeniy Petrov <evgeniy161102@gmail.com>
2024-04-15 09:57:26 +0300

    Сверстан слайдер, и немного карточки товаров

--- src/components/ProducCard/ProductCard.jsx
+++ src/components/ProducCard/ProductCard.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "./ProductCard.module.scss";
+import { IoIosStarOutline } from "react-icons/io";
+import { PiChartBarDuotone } from "react-icons/pi";
 
 const ProductCard = ({ product }) => {
   return (
     <>
       <div className={styles["card"]}>
-        <div className="card__cover">
+        <nav className={styles["card__nav"]}>
+          <button className={styles["card__btn-chart"]}>
+            <PiChartBarDuotone size={30} />
+          </button>
+          <button className={styles["card__btn-star"]}>
+            <IoIosStarOutline size={30} />
+          </button>
+        </nav>
+        <div className={styles["card__cover"]}>
           <img src={product.imageUrl} />
         </div>
         <h1 className={styles["card__title"]}>{product.name}</h1>
--- src/pages/HomePage/sections/PopularProducts/PopularProducts.jsx
+++ src/pages/HomePage/sections/PopularProducts/PopularProducts.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import styles from "./PopularProducts.module.scss";
-import Slider from "react-slick";
 import ProductCard from "../../../../components/ProducCard/ProductCard";
+import Slider from "react-slick";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const PopularProducts = () => {
   const settings = {
+    className: "center",
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    // cssEase: "linear",
+    // nexArrow: true,
+    // prevArrow: true,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true,
+        },
+      },
+    ],
   };
 
   const products = [
@@ -46,6 +62,39 @@ const PopularProducts = () => {
       weekPrice: 4380,
       monthPrice: 12250,
     },
+    {
+      id: 4,
+      imageUrl:
+        "https://c.dns-shop.ru/thumb/st4/fit/320/250/fbd7d085192ebe3eb518905892fddb3d/a6965dcbddcec201c585a38bfdc5f489c8d2621bca568fb91ce1231f088e8abd.jpg",
+      name: "Canon EOS R100 Kit",
+      companyName: "Canon",
+      workDayPrice: 630,
+      freeDayPrice: 750,
+      weekPrice: 4380,
+      monthPrice: 12250,
+    },
+    {
+      id: 5,
+      imageUrl:
+        "https://c.dns-shop.ru/thumb/st4/fit/320/250/fbd7d085192ebe3eb518905892fddb3d/a6965dcbddcec201c585a38bfdc5f489c8d2621bca568fb91ce1231f088e8abd.jpg",
+      name: "Canon EOS R100 Kit",
+      companyName: "Canon",
+      workDayPrice: 630,
+      freeDayPrice: 750,
+      weekPrice: 4380,
+      monthPrice: 12250,
+    },
+    {
+      id: 6,
+      imageUrl:
+        "https://c.dns-shop.ru/thumb/st4/fit/320/250/fbd7d085192ebe3eb518905892fddb3d/a6965dcbddcec201c585a38bfdc5f489c8d2621bca568fb91ce1231f088e8abd.jpg",
+      name: "Canon EOS R100 Kit",
+      companyName: "Canon",
+      workDayPrice: 630,
+      freeDayPrice: 750,
+      weekPrice: 4380,
+      monthPrice: 12250,
+    },
   ];
   return (
     <>
@@ -58,9 +107,11 @@ const PopularProducts = () => {
             товары
           </h2>
           <div className={styles["popular-products-row"]}>
-            {products.map((product) => (
-              <ProductCard product={product} />
-            ))}
+            <Slider className={styles["slider-wrapper"]} {...settings}>
+              {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+            </Slider>
           </div>
         </div>
       </section>
